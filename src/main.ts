98915cf5ae3ca05db7e7713a5dd2a54f5f67503a
Vovarama1992 <vovayhh9988@gmail.com>
2025-01-26 13:13:58 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'http://127.0.0.1:3000',
-      'https://app.opticard.co',
-      'http://127.0.0.1:5173',
-    ],
+    origin: ['http://localhost:3000', 'https://app.opticard.co'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
